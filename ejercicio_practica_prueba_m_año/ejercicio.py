c187472184bce068dabf86820c146f727fc32f47
DarvX01 <104405925+Dreyco0001@users.noreply.github.com>
2023-06-20 13:15:39 -0400

    fixs and update
    update the hard wok

--- ejercicio_practica_prueba_m_año/ejercicio.py
+++ ejercicio_practica_prueba_m_año/ejercicio.py
@@ -2,9 +2,26 @@
 #recordar IVA 19%
 #menu con opciones 1_ver /2_reserva/3_carta/4_pagar/5_calcelar/6_salir
 
+'''carta de la casa
+                        _________________________
+                        Bibidas (tamaños de cada bebible consulte con el mesero en turno)
+                        1_cerveza
+                        2_jugo
+                        3_Café
+                        _____________________
+                        Platos by Juan
+                        1_Ensalada de mariscos
+                        2_Ramén
+                        3_Pique macho
+                        _______________________
+                        Postres
+                        1_helado
+                        2_yogurt
+                        3_Flan especial de la casa
+                        '''
 #---------------------------------------
 import numpy as np
-import funciones_comun
+import funciones_comun as fc    
 from funciones_comun import *
 #---------------------------------------
 
@@ -46,68 +63,39 @@ colupna={}
 
 while True:
     print("welcome a pollos hermanos Restaurant.")
-    while True:
-        try:
-            print('''
-                  \t Menú 
-                  1_ver
-                  2_Reserva
-                  3_Carte
-                  4_Pagar
-                  5_cancelar
-                  6_cierre''')
-            menu_ops_P=int(input("Ingrese opcion deseada"))
-            if menu_ops_P in(1,2,3,4,5,6):
-                break
-            else:
-                print("ERROR ingrese un número valido")
-        except:
-            print("ERROR ingrese un nro entero")
-        if menu_ops_P==1:#ver mesas libres
-            pass
-        elif menu_ops_P==2:#ver reserva
-            pass
-        elif menu_ops_P==3:#carta
-            rut=rut_validar
-            if rut in Ruts:
-                posicion=rut_located
-            else:
-                print("proceso de pago de mesa activado")
-                continue
-            print(f'''carta de la casa
-                    _________________________
-                    Bibidas (tamaños de cada bebible consulte con el mesero en turno)
-                    1_cerveza
-                    2_jugo
-                    3_Café
-                    _____________________
-                    Platos by Juan
-                    1_Ensalada de mariscos
-                    2_Ramén
-                    3_Pique macho
-                    _______________________
-                    Postres
-                    1_helado
-                    2_yogurt
-                    3_Flan especial de la casa
-                    ''')
-        elif menu_ops_P==4:#pagar
-            rut=rut_validar
-            if rut in Ruts:
-                print("proceso de pago de mesa activado")
-                posicion=rut_located
-            else:
-                continue
-                
-        elif menu_ops_P==5:#cancelar
-            rut=rut_validar
-            if rut in Ruts:
-                continue
-            else:
-                print("Calcelar reserva")
-                posicion=rut_located
-                
-        else:#Cierre sistema
+    fc.mostrar_menu
+    menu_ops_P=fc.validar_opciones_menu
+    if menu_ops_P==1:#ver mesas libres
+        pass
+    elif menu_ops_P==2:#ver reserva
+        pass
+    elif menu_ops_P==3:#carta
+        rut=rut_validar
+        if rut in Ruts:
+            posicion=rut_located
+        else:
+            print("proceso de pago de mesa activado")
+            continue
+        fc.opc_carta
+        
+        menu_carta=fc.validacion_carta
+    elif menu_ops_P==4:#pagar
+        rut=rut_validar
+        if rut in Ruts:
+            print("proceso de pago de mesa activado")
+            posicion=rut_located
+        else:
+            continue
+            
+    elif menu_ops_P==5:#cancelar
+        rut=rut_validar
+        if rut in Ruts:
+            continue
+        else:
+            print("Calcelar reserva")
+            posicion=rut_located
+            
+    else:#Cierre sistema
             while True:
                 try:
                     cierre=int(input("¿Seguro que quiere cerrar?(si=1/no=2) _"))
--- ejercicio_practica_prueba_m_año/funciones_comun.py
+++ ejercicio_practica_prueba_m_año/funciones_comun.py
@@ -1,4 +1,17 @@
 #funciones que más estamos ocupando
+import os
+import numpy
+import msvcrt
+restaurant=numpy.zeros((3.3),int)
+
+
+#crear arreglo y como acomodar a gusto
+def ver_mesas ():
+    for x in range(3):
+        for y in range(3):
+            print(restaurant[x][y], end=" ")
+        print()
+
 
 def rut_validar(rut):
     while True:
@@ -12,7 +25,60 @@ def rut_validar(rut):
         except:
             print("ERROR rut ingresado no valido")
             
+def opc_carta (menu_carta)            
+     print(f'''
+                        carta de la casa
+                    _________________________
+                    1) Bibidas (tamaños de cada bebible consulte con el mesero en turno)
+                    1_cerveza
+                    2_jugo
+                    3_Café
+                    _____________________
+                    2) Platos by Juan
+                    1_Ensalada de mariscos
+                    2_Ramén
+                    3_Pique macho
+                    _______________________
+                    3) Postres
+                    1_helado
+                    2_yogurt
+                    3_Flan especial de la casa
+                        ''')
+     
+     
+def validacion_carta (menu_carta):
+    while True:
+        try:
+            menu_carta=int(input("Ingrese opcion deseada"))
+            if menu_carta in(1,2,3):
+                return menu_carta
+            else:
+                print("ERROR ingrese un número valido")
+        except:
+            print("ERROR ingrese un nro entero") 
+
             
+def mostrar_menu ():
+    print('''
+                \t Menú 
+                1_ver
+                2_Reserva
+                3_Carte
+                4_Pagar
+                5_cancelar
+                6_cierre''')
+
+def validar_opciones_menu (menu_ops_P):
+     while True:
+        try:
+            menu_ops_P=int(input("Ingrese opcion deseada"))
+            if menu_ops_P in(1,2,3,4,5,6):
+                return menu_ops_P
+            else:
+                print("ERROR ingrese un número valido")
+        except:
+            print("ERROR ingrese un nro entero")            
+                
 def validar_nombre (P_nombre):
     while True:
         P_nombre=input("ingrese Nombre")
